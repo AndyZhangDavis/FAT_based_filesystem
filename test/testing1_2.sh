cbe4530fd9a02c36eb814226cdd2c6975fa5b48f
waiyu <waiyu0616@outlook.com>
2019-11-23 16:38:10 -0800

    add testing script for phase 1 and 2

--- test/testing1_2.sh
+++ test/testing1_2.sh
@@ -7,6 +7,33 @@
 # get fs_info from my lib
 ./test_fs.x info disk.fs >lib.stdout 2>lib.stderr
 
+# create 5 files onto host 
+for i in $(seq -w 1 5); do echo "hello world" > test${i}; done
+# add 5 files to disk.fs
+for i in $(seq -w 1 5); do ./fs_ref.x add disk.fs test${i}; done
+# remove file to disk.fs
+./fs_ref.x rm disk.fs file03
+./fs_ref.x info disk.fs >ref.stdout 2>ref.stderr
+./fs_ref.x ls disk.fs >ref.stdout 2>ref.stderr
+# add file 06 to disk.fs
+./fs_ref.x add disk.fs test06
+./fs_ref.x info disk.fs >ref.stdout 2>ref.stderr
+./fs_ref.x ls disk.fs >ref.stdout 2>ref.stderr
+# Remove all files in disk.fs and execute same operation in our program to compare the result 
+for i in $(seq -w 1 6); do ./fs_ref.x rm disk.fs file${i}; done
+# add 5 files to disk.fs
+for i in $(seq -w 1 5); do ./test_fs.x add disk.fs test${i}; done
+# remove file to disk.fs
+./test_fs.x rm disk.fs file03
+./test_fs.x info disk.fs >lib.stdout 2>lib.stderr
+./test_fs.x ls disk.fs >lib.stdout 2>lib.stderr
+# add file 06 to disk.fs
+./test_fs.x add disk.fs test06
+./test_fs.x info disk.fs >lib.stdout 2>lib.stderr
+./fs_ref.x ls disk.fs >lib.stdout 2>lib.stderr
+# remove all files
+for i in $(seq -w 1 6); do ./test_fs.x rm disk.fs file${i}; done
+
 # put output files into variables
 REF_STDOUT=$(cat ref.stdout)
 REF_STDERR=$(cat ref.stderr)
@@ -33,3 +60,4 @@ fi
 rm disk.fs
 rm ref.stdout ref.stderr
 rm lib.stdout lib.stderr
+for i in $(seq -w 1 6); do rm file${i}; done
